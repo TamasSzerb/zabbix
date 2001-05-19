97b1dd9e47d766c289a7561c5a43d8971359aeb8
nobody <nobody@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2001-05-19 08:17:29 +0000

    This commit was manufactured by cvs2svn to create tag 'zabbix-1_0alpha3'.

---
+++ tags/zabbix-1_0alpha3/include/common.h
@@ -0,0 +1,20 @@
+#ifndef MON_COMMON_H
+#define MON_COMMON_H
+ 
+#define	SUCCEED		0
+#define	FAIL		(-1)
+#define	NOTSUPPORTED	(-2)
+
+#define	MAXFD	64
+
+
+#define	SUCKER_DELAY	10
+#define	ALARMER_DELAY	15
+
+#define	SUCKER_TIMEOUT	5
+#define	AGENT_TIMEOUT	3
+
+#define	SENDER_TIMEOUT	3
+#define	TRAPPER_TIMEOUT	5
+
+#endif

d65f0860b294253e55df0473f8b709c3711b651f
nobody <nobody@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-05-29 08:28:29 +0000

    This commit was manufactured by cvs2svn to create tag 'zabbix-1_1alpha10'.

---
+++ tags/zabbix-1_1alpha10/include/cfg.h
@@ -0,0 +1,42 @@
+/* 
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+
+#ifndef ZABBIX_CFG_H
+#define ZABBIX_CFG_H
+
+#define	TYPE_INT	0
+#define	TYPE_STRING	1
+
+#define	PARM_OPT	0
+#define	PARM_MAND	1
+
+struct cfg_line
+{
+	char	*parameter;
+	void	*variable;
+	int	(*function)();
+	int	type;
+	int	mandatory;
+	int	min;
+	int	max;
+};
+
+int	parse_cfg_file(char *cfg_file,struct cfg_line *cfg);
+
+#endif

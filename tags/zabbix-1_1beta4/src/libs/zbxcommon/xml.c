f17e67abc277ba576d3f7770fcbd56457a2fd390
nobody <nobody@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2005-12-11 09:07:15 +0000

    This commit was manufactured by cvs2svn to create tag 'zabbix-1_1beta4'.

---
+++ tags/zabbix-1_1beta4/src/libs/zbxcommon/xml.c
@@ -0,0 +1,47 @@
+#include "common.h"
+#include "log.h"
+
+#include <stdio.h>
+#include <string.h>
+
+/* Get DATA from <tag>DATA</tag> */
+int xml_get_data(char *xml,char *tag, char *data, int maxlen)
+{
+	int ret = SUCCEED;
+	char *start, *end;
+	char tag_open[MAX_STRING_LEN];
+	char tag_close[MAX_STRING_LEN];
+	int len;
+
+	snprintf(tag_open,MAX_STRING_LEN-1,"<%s>",tag);
+	snprintf(tag_close,MAX_STRING_LEN-1,"</%s>",tag);
+
+	if(NULL==(start=strstr(xml,tag_open)))
+	{
+		ret = FAIL;
+	}
+
+	if(NULL==(end=strstr(xml,tag_close)))
+	{
+		ret = FAIL;
+	}
+
+	if(ret == SUCCEED)
+	{
+		if(end<start)
+		{
+			ret = FAIL;
+		}
+	}
+
+	if(ret == SUCCEED)
+	{
+		len=end-(start+strlen(tag_open));
+
+		if(len>maxlen)	len=maxlen;
+
+		strncpy(data, start+strlen(tag_open),len);
+	}
+
+	return ret;
+}

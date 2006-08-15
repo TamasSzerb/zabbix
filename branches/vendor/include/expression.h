edeb719c901952b3306329bf8fc1bfc68e4debad
eugene <eugene@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-08-15 10:43:22 +0000

    removed branch

--- branches/vendor/include/expression.h
+++
@@ -1,8 +0,0 @@
-#ifndef MON_EXPRESSION_H
-#define MON_EXPRESSION_H
-
-int	FindChar(char *str,char c);
-int	SubstituteFunctions(char *exp);
-int     EvaluateExpression (int *Result,char *expression);
-
-#endif

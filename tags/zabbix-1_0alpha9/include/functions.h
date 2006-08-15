b936c54c80255025578647180d4fbf9722814ddd
eugene <eugene@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-08-15 10:53:18 +0000

    removed tag

--- tags/zabbix-1_0alpha9/include/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef MON_FUNCTIONS_H
-#define MON_FUNCTIONS_H
-
-int	update_functions( int itemid );
-void    update_triggers(int itemid);
-int	get_lastvalue(float *Result,char *host,char *key,char *function,char *parameter);
-
-#endif

1083df07a257027a4874a33645cdadc20a737259
eugene <eugene@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2006-08-15 10:52:57 +0000

    removed tag

--- tags/zabbix-1_0alpha3/include/expression.h
+++
@@ -1,8 +0,0 @@
-#ifndef MON_EXPRESSION_H
-#define MON_EXPRESSION_H
-
-int	find_char(char *str,char c);
-int	substitute_functions(char *exp);
-int     evaluate_expression (int *result,char *expression);
-
-#endif

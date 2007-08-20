3496f1636218339761b664bb199fcde021c08949
alex <alex@f4e7fd02-d210-4e34-a1e0-d4093fb66cc2>
2007-08-20 19:18:00 +0000

    New tag.

---
+++ tags/1.4.2/frontends/php/events.php
@@ -0,0 +1,189 @@
+<?php
+/* 
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+	require_once "include/config.inc.php";
+	require_once "include/hosts.inc.php";
+	require_once "include/events.inc.php";
+	require_once "include/discovery.inc.php";
+
+	$page["title"] = "S_LATEST_EVENTS";
+	$page["file"] = "events.php";
+	
+	define('ZBX_PAGE_DO_REFRESH', 1);
+
+include_once "include/page_header.php";
+
+?>
+<?php
+	$allow_discovery = check_right_on_discovery(PERM_READ_ONLY);
+
+	$allowed_sources[] = EVENT_SOURCE_TRIGGERS;
+	if($allow_discovery)
+		$allowed_sources[] = EVENT_SOURCE_DISCOVERY;
+
+	define('PAGE_SIZE',	100);
+//		VAR			TYPE	OPTIONAL FLAGS	VALIDATION	EXCEPTION
+	$fields=array(
+		"source"=>			array(T_ZBX_INT, O_OPT,	P_SYS,	IN($allowed_sources),	NULL),
+		"show_unknown"=>	array(T_ZBX_INT, O_OPT,	P_SYS,	IN(array(0,1)),	NULL),
+		"groupid"=>			array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL),
+		"hostid"=>			array(T_ZBX_INT, O_OPT,	P_SYS,	DB_ID,	NULL),
+		"start"=>			array(T_ZBX_INT, O_OPT,	P_SYS,	BETWEEN(0,65535)."({}%".PAGE_SIZE."==0)",	NULL),
+		"next"=>			array(T_ZBX_STR, O_OPT,	P_SYS,	NULL,			NULL),
+		"prev"=>			array(T_ZBX_STR, O_OPT,	P_SYS,	NULL,			NULL)
+	);
+
+	$_REQUEST['source'] = get_request('source', get_profile('web.events.source', 0));
+	
+	check_fields($fields);
+
+	$source = get_request('source', EVENT_SOURCE_TRIGGERS);
+	$show_unknown = get_request('show_unknown',get_profile('web.events.show_unknown',0));
+
+	update_profile('web.events.source',$source);
+	update_profile('web.events.show_unknown',$show_unknown);
+
+?>
+<?php
+	$_REQUEST["start"] = get_request("start", 0);
+	if(isset($_REQUEST["prev"]))
+	{
+		$_REQUEST["start"] -= PAGE_SIZE;
+	}
+	if(isset($_REQUEST["next"]))
+	{
+		$_REQUEST["start"]	+= PAGE_SIZE;
+	}
+	if($_REQUEST["start"] < 0) $_REQUEST["start"] = 0;
+?>
+<?php
+	$source = get_request('source', EVENT_SOURCE_TRIGGERS);
+
+	$r_form = new CForm();
+	$r_form->AddOption('name','events_menu');
+
+	if($source == EVENT_SOURCE_TRIGGERS){
+		$chkbox = new CCheckBox('sh_unknown',
+							(($show_unknown == 0)?'no':'yes'),
+							'create_var("events_menu", "show_unknown", '.(($show_unknown == 0)?'1':'0').', true)'
+							);
+		$r_form->AddItem(array(S_SHOW_UNKNOWN, SPACE, $chkbox, SPACE, SPACE));
+	}
+	
+	if($allow_discovery)
+	{
+		$cmbSource = new CComboBox('source', $source, 'submit()');
+		$cmbSource->AddItem(EVENT_SOURCE_TRIGGERS, S_TRIGGER);
+		$cmbSource->AddItem(EVENT_SOURCE_DISCOVERY, S_DISCOVERY);
+		$r_form->AddItem(array(S_SOURCE, SPACE, $cmbSource));
+	}
+	show_table_header(S_HISTORY_OF_EVENTS_BIG,$r_form);
+	echo BR;
+
+	$r_form = new CForm();
+
+	if($source == EVENT_SOURCE_DISCOVERY)
+	{
+		$table = get_history_of_discovery_events($_REQUEST["start"], PAGE_SIZE);
+	}
+	else
+	{
+		validate_group_with_host(PERM_READ_ONLY, array("allow_all_hosts","monitored_hosts","with_items"));
+
+		$table = get_history_of_triggers_events($_REQUEST["start"], PAGE_SIZE, $_REQUEST["groupid"],$_REQUEST["hostid"]);
+
+		$cmbGroup = new CComboBox("groupid",$_REQUEST["groupid"],"submit()");
+		$cmbHosts = new CComboBox("hostid",$_REQUEST["hostid"],"submit()");
+
+		$cmbGroup->AddItem(0,S_ALL_SMALL);
+		
+		$availiable_hosts = get_accessible_hosts_by_user($USER_DETAILS, PERM_READ_LIST, null, null, get_current_nodeid());
+
+		$result=DBselect('SELECT DISTINCT g.groupid,g.name '.
+			' FROM groups g, hosts_groups hg, hosts h, items i '.
+			' WHERE h.hostid in ('.$availiable_hosts.') '.
+				' AND hg.groupid=g.groupid AND h.status='.HOST_STATUS_MONITORED.
+				' AND h.hostid=i.hostid AND hg.hostid=h.hostid '.
+			' ORDER BY g.name');
+
+		while($row=DBfetch($result))
+		{
+			$cmbGroup->AddItem(
+					$row['groupid'],
+					get_node_name_by_elid($row['groupid']).$row['name']
+					);
+		}
+		$r_form->AddItem(array(S_GROUP.SPACE,$cmbGroup));
+		
+		$cmbHosts->AddItem(0,S_ALL_SMALL);
+		if($_REQUEST['groupid'] > 0)
+		{
+			$sql='SELECT h.hostid,h.host '.
+				' FROM hosts h,items i,hosts_groups hg '.
+				' WHERE h.status='.HOST_STATUS_MONITORED.
+					' AND h.hostid=i.hostid AND hg.groupid='.$_REQUEST['groupid'].
+					' AND hg.hostid=h.hostid AND h.hostid in ('.$availiable_hosts.') '.
+				' GROUP BY h.hostid,h.host '.
+				' ORDER BY h.host';
+		}
+		else
+		{
+			$sql='SELECT h.hostid,h.host '.
+				' FROM hosts h,items i '.
+				' WHERE h.status='.HOST_STATUS_MONITORED.
+					' AND h.hostid=i.hostid'.
+					' AND h.hostid in ('.$availiable_hosts.') '.
+				' GROUP BY h.hostid,h.host '.
+				' ORDER BY h.host';
+		}
+		$result=DBselect($sql);
+		while($row=DBfetch($result))
+		{
+			$cmbHosts->AddItem(
+					$row['hostid'],
+					get_node_name_by_elid($row['hostid']).$row['host']
+					);
+		}
+
+		$r_form->AddItem(array(SPACE.S_HOST.SPACE,$cmbHosts));
+	}
+	
+	$r_form->AddVar('start',$_REQUEST['start']);
+
+	$btnPrev = new CButton("prev","<< Prev ".PAGE_SIZE);
+	if($_REQUEST["start"] <= 0)
+		$btnPrev->SetEnabled('no');
+	$r_form->AddItem($btnPrev);
+
+	$btnNext = new CButton("next","Next ".PAGE_SIZE." >>");
+	if($table->GetNumRows() < PAGE_SIZE)
+		$btnNext->SetEnabled('no');
+	$r_form->AddItem($btnNext);
+	
+	show_table_header(S_EVENTS_BIG,$r_form);
+
+        $table->Show();
+?>
+<?php
+
+include_once "include/page_footer.php";
+
+?>
---
+++ tags/1.4.2/frontends/php/include/events.inc.php
@@ -0,0 +1,208 @@
+<?php
+/*
+** ZABBIX
+** Copyright (C) 2000-2005 SIA Zabbix
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**/
+?>
+<?php
+	function	event_source2str($sourceid)
+	{
+		switch($sourceid)
+		{
+			case EVENT_SOURCE_TRIGGERS:	return S_TRIGGERS;
+			case EVENT_SOURCE_DISCOVERY:	return S_DISCOVERY;
+			default:			return S_UNKNOWN;
+		}
+	}
+
+	function	get_history_of_triggers_events($start,$num, $groupid=0, $hostid=0)
+	{
+		global $USER_DETAILS;
+		
+		$show_unknown = get_profile('web.events.show_unknown',0);
+		
+		$sql_from = $sql_cond = "";
+		
+		if($hostid > 0)
+		{
+			$sql_cond = " and h.hostid=".$hostid;
+		}
+		elseif($groupid > 0)
+		{
+			$sql_from = ", hosts_groups hg ";
+			$sql_cond = " and h.hostid=hg.hostid and hg.groupid=".$groupid;
+		}
+
+		if($show_unknown == 0){
+			$sql_cond.= ' AND e.value<>2 ';
+		}
+	
+		$result = DBselect('SELECT DISTINCT t.triggerid,t.priority,t.description,h.host,e.clock,e.value '.
+			' FROM events e, triggers t, functions f, items i, hosts h '.$sql_from.
+			' WHERE '.DBin_node('t.triggerid').
+				' AND e.objectid=t.triggerid and e.object='.EVENT_OBJECT_TRIGGER.
+				' AND t.triggerid=f.triggerid and f.itemid=i.itemid '.
+				' AND i.hostid=h.hostid '.$sql_cond.' and h.status='.HOST_STATUS_MONITORED.
+			' ORDER BY e.clock DESC,h.host,t.priority,t.description,t.triggerid ',10*($start+$num)
+			);
+       
+		$table = new CTableInfo(S_NO_EVENTS_FOUND); 
+		$table->SetHeader(array(
+				S_TIME,
+				is_show_subnodes() ? S_NODE : null,
+				$hostid == 0 ? S_HOST : null,
+				S_DESCRIPTION,
+				S_VALUE,
+				S_SEVERITY
+				));
+		
+		$accessible_hosts = get_accessible_hosts_by_user($USER_DETAILS,PERM_READ_ONLY);
+		
+		$col=0;
+		$skip = $start;
+
+		while(($row=DBfetch($result)) && ($col<$num)){
+			
+			if($skip > 0){
+				$skip--;
+				continue;
+			}
+			
+			if($row["value"] == 0)
+			{
+				$value=new CCol(S_OFF,"off");
+			}
+			elseif($row["value"] == 1)
+			{
+				$value=new CCol(S_ON,"on");
+			}
+			else
+			{
+				$value=new CCol(S_UNKNOWN_BIG,"unknown");
+			}	
+			if(($show_unknown == 0) && (!event_initial_time($row,$show_unknown))) continue;
+				
+			$table->AddRow(array(
+				date("Y.M.d H:i:s",$row["clock"]),
+				get_node_name_by_elid($row['triggerid']),
+				$hostid == 0 ? $row['host'] : null,
+				new CLink(
+					expand_trigger_description_by_data($row),
+					"tr_events.php?triggerid=".$row["triggerid"],"action"
+					),
+				$value,
+				new CCol(get_severity_description($row["priority"]), get_severity_style($row["priority"]))));
+			$col++;
+		}
+		return $table;
+	}
+
+	function	get_history_of_discovery_events($start,$num)
+	{
+		$db_events = DBselect('select distinct e.source,e.object,e.objectid,e.clock,e.value from events e'.
+			' where e.source='.EVENT_SOURCE_DISCOVERY.' order by e.clock desc',
+			10*($start+$num)
+			);
+       
+		$table = new CTableInfo(S_NO_EVENTS_FOUND); 
+		$table->SetHeader(array(S_TIME, S_IP, S_DESCRIPTION, S_STATUS));
+		$col=0;
+		
+		$skip = $start;
+		while(($event_data = DBfetch($db_events))&&($col<$num))
+		{
+			if($skip > 0) 
+			{
+				$skip--;
+				continue;
+			}
+
+			if($event_data["value"] == 0)
+			{
+				$value=new CCol(S_UP,"off");
+			}
+			elseif($event_data["value"] == 1)
+			{
+				$value=new CCol(S_DOWN,"on");
+			}
+			else
+			{
+				$value=new CCol(S_UNKNOWN_BIG,"unknown");
+			}
+
+
+			switch($event_data['object'])
+			{
+				case EVENT_OBJECT_DHOST:
+					$object_data = DBfetch(DBselect('select ip from dhosts where dhostid='.$event_data['objectid']));
+					$description = SPACE;
+					break;
+				case EVENT_OBJECT_DSERVICE:
+					$object_data = DBfetch(DBselect('select h.ip,s.type,s.port from dhosts h,dservices s '.
+						' where h.dhostid=s.dhostid and s.dserviceid='.$event_data['objectid']));
+					$description = S_SERVICE.': '.discovery_check_type2str($object_data['type']).'; '.
+						S_PORT.': '.$object_data['port'];
+					break;
+				default:
+					continue;
+			}
+
+			if(!$object_data) continue;
+
+
+			$table->AddRow(array(
+				date("Y.M.d H:i:s",$event_data["clock"]),
+				$object_data['ip'],
+				$description,
+				$value));
+
+			$col++;
+		}
+		return $table;
+	}
+	
+function event_initial_time($row,$show_unknown=0){
+	$sql_cond=($show_unknown == 0)?' AND value<>2 ':'';
+
+	$events = array();
+	$res = DBselect('SELECT MAX(clock) as clock, value '.
+					' FROM events '.
+					' WHERE objectid='.$row['triggerid'].$sql_cond.
+						' AND clock < '.$row['clock'].
+					' GROUP BY value '.
+					' ORDER BY clock DESC');
+					
+	while($rows = DBfetch($res)){
+		$events[] = $rows;
+	}
+	if(!empty($events) && ($events[0]['value'] == $row['value'])){
+
+		$clock = (count($events) > 1)?($events[1]['clock']):(0);
+		$res = DBselect('SELECT MIN(clock) as clock, value '.
+						' FROM events as e '.
+						' WHERE clock > '.$clock.$sql_cond.
+							' AND objectid='.$row['triggerid'].
+							' AND clock < '.$row['clock'].
+						' GROUP BY value');
+		while($rows = DBfetch($res)){
+			$rclock = $rows['clock'];
+		}
+		if($rclock != $row['clock']) return false;
+	}
+	return true;
+}
+?>

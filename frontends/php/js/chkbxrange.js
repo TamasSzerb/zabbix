TamasSzerb/zabbix

commit 872026c3b99deb876a3d07b90475893c96382639

..F....... [ZBXNEXT-570] implemented the ability to quickly display items on a graph from the latest data page
- It is now possible to select several items on the latest data page and quickly display them all on a single graph. The filter on the latest data page has been changed to allow selecting multiple hosts and host groups at once, as well as filtering by application. The latest data page will now only display values if at least some filter criteria has been selected.

git-svn-id: svn://svn.zabbix.com/trunk@48423 f4e7fd02-d210-4e34-a1e0-d4093fb66cc2

frontends/php/js/chkbxrange.js
@@ -1,0 +1,326 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2014 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+/*
+ * Automatic checkbox range selection
+ */
+var chkbxRange = {
+	startbox:		null,	// start checkbox obj
+	chkboxes:		{},		// ckbx list
+	prefix:			null,	// prefix for cookie name
+	pageGoName:		null,	// which checkboxes should be counted by Go button and saved to cookies
+	selectedIds:	{},		// ids of selected objects
+	goButton:		null,
+	cookieName:		null,
+
+	init: function() {
+		// cookie name
+		var path = new Curl();
+		var filename = basename(path.getPath(), '.php');
+		this.cookieName = 'cb_' + filename + (this.prefix ? '_' + this.prefix : '');
+
+		this.resetOtherPageCookies();
+
+		// initialize checkboxes
+		var chkboxes = jQuery('.tableinfo .checkbox:not(:disabled)');
+		if (chkboxes.length > 0) {
+			for (var i = 0; i < chkboxes.length; i++) {
+				this.implement(chkboxes[i]);
+			}
+		}
+
+		// load selected checkboxes from cookies or cache
+		if (this.pageGoName != null) {
+			this.selectedIds = cookie.readJSON(this.cookieName);
+
+			// check if checkboxes should be selected from cookies
+			if (!jQuery.isEmptyObject(this.selectedIds)) {
+				var objectIds = jQuery.map(this.selectedIds, function(id) { return id });
+			}
+			// no checkboxes selected from cookies, check browser cache if checkboxes are still checked and update state
+			else {
+				var checkedFromCache = jQuery('.tableinfo tr:not(.header) .checkbox:checked:not(:disabled)');
+				var objectIds = jQuery.map(checkedFromCache, jQuery.proxy(function(checkbox) {
+					return this.getObjectIdFromName(checkbox.name);
+				}, this));
+			}
+
+			this.checkObjects(this.pageGoName, objectIds, true);
+			this.update(this.pageGoName);
+		}
+
+		// bind event to the "Go" button
+		this.goButton = $('goButton');
+		if (!is_null(this.goButton)) {
+			addListener(this.goButton, 'click', this.submitGo.bindAsEventListener(this), false);
+		}
+	},
+
+	implement: function(obj) {
+		// skip the "select all" checkbox
+		if (obj.name.indexOf('all_') > -1) {
+			return;
+		}
+
+		var objName = this.getObjectFromName(obj.name);
+
+		if (typeof(this.chkboxes[objName]) === 'undefined') {
+			this.chkboxes[objName] = [];
+		}
+		this.chkboxes[objName].push(obj);
+
+		addListener(obj, 'click', this.handleClick.bindAsEventListener(this), false);
+
+		if (objName == this.pageGoName) {
+			var objId = jQuery(obj).val();
+			if (isset(objId, this.selectedIds)) {
+				obj.checked = true;
+			}
+		}
+	},
+
+	/**
+	 * Handles a click on one of the checkboxes.
+	 *
+	 * @param e
+	 */
+	handleClick: function(e) {
+		e = e || window.event;
+		var checkbox = Event.element(e);
+
+		PageRefresh.restart();
+
+		var object = this.getObjectFromName(checkbox.name);
+		var objectId = this.getObjectIdFromName(checkbox.name);
+
+		// range selection
+		if ((e.ctrlKey || e.shiftKey) && this.startbox != null) {
+			this.checkObjectRange(object, this.startbox, checkbox, this.startbox.checked);
+		}
+		// an individual checkbox
+		else {
+			this.checkObjects(object, [objectId], checkbox.checked);
+		}
+
+		this.update(object);
+		this.saveCookies(object);
+
+		this.startbox = checkbox;
+	},
+
+	/**
+	 * Extracts the name of an object from the name of a checkbox.
+	 *
+	 * @param {string} name
+	 *
+	 * @returns {string}
+	 */
+	getObjectFromName: function(name) {
+		return name.split('[')[0];
+	},
+
+	/**
+	 * Extracts the ID of an object from the name of a checkbox.
+	 *
+	 * @param {string} name
+	 *
+	 * @returns {string}
+	 */
+	getObjectIdFromName: function(name) {
+		var id = name.split('[')[1];
+		id = id.substring(0, id.lastIndexOf(']'));
+
+		return id;
+	},
+
+	/**
+	 * Toggle all checkboxes of the given objects.
+	 *
+	 * Checks all of the checkboxes that belong to these objects and highlights the table row.
+	 *
+	 * @param {string}  object
+	 * @param {Array}   objectIds     array of objects IDs as integers
+	 * @param {bool}    checked
+	 */
+	checkObjects: function(object, objectIds, checked) {
+		jQuery.each(this.chkboxes[object], jQuery.proxy(function(i, checkbox) {
+			var objectId = this.getObjectIdFromName(checkbox.name);
+
+			if (objectIds.indexOf(objectId) > -1) {
+				checkbox.checked = checked;
+
+				jQuery(checkbox).closest('tr').toggleClass('selected', checked);
+
+				if (checked) {
+					this.selectedIds[objectId] = objectId;
+				}
+				else {
+					delete this.selectedIds[objectId];
+				}
+			}
+		}, this));
+	},
+
+	/**
+	 * Toggle all objects between the two checkboxes.
+	 *
+	 * @param {string} object
+	 * @param {object} startCheckbox
+	 * @param {object} endCheckbox
+	 * @param {bool} checked
+	 */
+	checkObjectRange: function(object, startCheckbox, endCheckbox, checked) {
+		var checkboxes = this.chkboxes[object];
+
+		var startCheckboxIndex = checkboxes.indexOf(startCheckbox);
+		var endCheckboxIndex = checkboxes.indexOf(endCheckbox);
+		var start = Math.min(startCheckboxIndex, endCheckboxIndex);
+		var end = Math.max(startCheckboxIndex, endCheckboxIndex);
+
+		var objectIds = [];
+		for (var i = start; i <= end; i++) {
+			objectIds.push(this.getObjectIdFromName(checkboxes[i].name));
+		}
+		this.checkObjects(object, objectIds, checked);
+
+	},
+
+	/**
+	 * Toggle all of the checkboxes belonging to the given object group.
+	 *
+	 * @param {string} object
+	 *
+	 * @param {bool} checked
+	 */
+	checkObjectAll: function(object, checked) {
+		// main checkbox exists and is clickable, but other checkboxes may not exist and object may be empty
+		if (typeof this.chkboxes[object] !== 'undefined') {
+			var objectIds = jQuery.map(this.chkboxes[object], jQuery.proxy(function(checkbox) {
+				return this.getObjectIdFromName(checkbox.name);
+			}, this));
+
+			this.checkObjects(object, objectIds, checked);
+		}
+	},
+
+	/**
+	 * Update the general state after toggling a checkbox.
+	 *
+	 * @param {string} object
+	 */
+	update: function(object) {
+		// update main checkbox state
+		this.updateMainCheckbox();
+
+		if (this.pageGoName == object) {
+			this.updateGoButton();
+		}
+	},
+
+	/**
+	 * Update the state of the "Go" controls.
+	 */
+	updateGoButton: function() {
+		var count = 0;
+		jQuery.each(this.selectedIds, function() {
+			count++;
+		});
+
+		// update go button
+		var goButton = jQuery('#goButton');
+		goButton.val(goButton.val().split(' ')[0] + ' (' + count + ')')
+			.prop('disabled', count == 0);
+		jQuery('#action').prop('disabled', count == 0);
+	},
+
+	// check if all checkboxes are selected and select main checkbox, else disable checkbox, select options and button
+	updateMainCheckbox: function() {
+		var mainCheckbox = jQuery('.tableinfo .header .checkbox:not(:disabled)');
+		if (!mainCheckbox.length) {
+			return;
+		}
+
+		var countAvailable = jQuery('.tableinfo tr:not(.header) .checkbox:not(:disabled)').length;
+
+		if (countAvailable > 0) {
+			var countChecked = jQuery('.tableinfo tr:not(.header) .checkbox:not(:disabled):checked').length;
+
+			mainCheckbox = mainCheckbox[0];
+			mainCheckbox.checked = (countChecked == countAvailable);
+
+			if (mainCheckbox.checked) {
+				jQuery('.tableinfo .header').addClass('selectedMain');
+			}
+			else {
+				jQuery('.tableinfo .header').removeClass('selectedMain');
+			}
+		}
+		else {
+			mainCheckbox.disabled = true;
+		}
+	},
+
+	/**
+	 * Save the state of the checkboxes belonging to the given object group in cookies.
+	 *
+	 * @param {string} object
+	 */
+	saveCookies: function(object) {
+		if (this.pageGoName == object) {
+			cookie.createJSON(this.cookieName, this.selectedIds);
+		}
+	},
+
+	clearSelectedOnFilterChange: function() {
+		cookie.eraseArray(this.cookieName);
+	},
+
+	/**
+	 * Reset all selections on other pages.
+	 */
+	resetOtherPageCookies: function() {
+		for (var key in cookie.cookies) {
+			var cookiePair = key.split('=');
+			if (cookiePair[0].indexOf('cb_') > -1 && cookiePair[0].indexOf(this.cookieName) == -1) {
+				cookie.erase(key);
+			}
+		}
+	},
+
+	submitGo: function(e) {
+		e = e || window.event;
+
+		var goSelect = $('go');
+		var confirmText = goSelect.options[goSelect.selectedIndex].getAttribute('confirm');
+
+		if (!is_null(confirmText) && !confirm(confirmText)) {
+			Event.stop(e);
+			return false;
+		}
+
+		var form = jQuery('#goButton').closest('form');
+		for (var key in this.selectedIds) {
+			if (!empty(this.selectedIds[key])) {
+				create_var(form.name, this.pageGoName + '[' + key + ']', key, false);
+			}
+		}
+		return true;
+	}
+};

frontends/php/auditacts.php
@@ -39,10 +39,6 @@ $fields = array(
 	'filter_set' =>	array(T_ZBX_STR, O_OPT, P_SYS,	null,	null),
 	'alias' =>		array(T_ZBX_STR, O_OPT, P_SYS,	null,	null),
 	'period' =>		array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'dec' =>		array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'inc' =>		array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'left' =>		array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'right' =>		array(T_ZBX_INT, O_OPT, null,	null,	null),
 	'stime' =>		array(T_ZBX_STR, O_OPT, null,	null,	null),
 	// ajax
 	'filterState' => array(T_ZBX_INT, O_OPT, P_ACT, null,	null),

frontends/php/auditlogs.php
@@ -40,10 +40,6 @@ $fields = array(
 	'filter_set' =>		array(T_ZBX_STR, O_OPT, P_SYS,	null,	null),
 	'alias' =>			array(T_ZBX_STR, O_OPT, P_SYS,	null,	null),
 	'period' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'dec' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'inc' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'left' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'right' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
 	'stime' =>			array(T_ZBX_STR, O_OPT, null,	null,	null),
 	// ajax
 	'filterState' =>	array(T_ZBX_INT, O_OPT, P_ACT,	null,	null),

frontends/php/chart.php
@@ -28,7 +28,8 @@ require_once dirname(__FILE__).'/include/page_header.php';
 
 // VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
 $fields = array(
-	'itemid' =>			array(T_ZBX_INT, O_MAND, P_SYS,	DB_ID,		null),
+	'type' =>           array(T_ZBX_INT, O_OPT, null,   IN(array(GRAPH_TYPE_NORMAL, GRAPH_TYPE_STACKED)), null),
+	'itemids' =>		array(T_ZBX_INT, O_MAND, P_SYS,	DB_ID,		null),
 	'period' =>			array(T_ZBX_INT, O_OPT, P_NZERO, BETWEEN(ZBX_MIN_PERIOD, ZBX_MAX_PERIOD), null),
 	'stime' =>			array(T_ZBX_STR, O_OPT, P_SYS,	null,		null),
 	'profileIdx' =>		array(T_ZBX_STR, O_OPT, null,	null,		null),
@@ -37,21 +38,35 @@ $fields = array(
 	'from' =>			array(T_ZBX_INT, O_OPT, null,	'{}>=0',	null),
 	'width' =>			array(T_ZBX_INT, O_OPT, null,	'{}>0',		null),
 	'height' =>			array(T_ZBX_INT, O_OPT, null,	'{}>0',		null),
-	'border' =>			array(T_ZBX_INT, O_OPT, null,	IN('0,1'),	null)
+	'border' =>			array(T_ZBX_INT, O_OPT, null,	IN('0,1'),	null),
+	'batch' =>			array(T_ZBX_INT, O_OPT, null,	IN('0,1'),	null),
 );
 check_fields($fields);
+
+$itemIds = getRequest('itemids');
 
 /*
  * Permissions
  */
-$dbItems = API::Item()->get(array(
-	'output' => array('itemid'),
-	'itemids' => $_REQUEST['itemid'],
+$items = API::Item()->get(array(
+	'output' => array('itemid', 'name'),
+	'selectHosts' => array('name'),
+	'itemids' => $itemIds,
 	'webitems' => true,
+	'preservekeys' => true
 ));
-if (!$dbItems) {
-	access_deny();
+foreach ($itemIds as $itemId) {
+	if (!isset($items[$itemId])) {
+		access_deny();
+	}
 }
+
+// sort items
+foreach ($items as &$item) {
+	$item['hostname'] = $item['hosts'][0]['name'];
+}
+unset($item);
+CArrayHelper::sort($items, array('name', 'hostname', 'itemid'));
 
 /*
  * Display
@@ -64,9 +79,18 @@ $timeline = CScreenBase::calculateTime(array(
 	'stime' => getRequest('stime')
 ));
 
-$graph = new CLineGraphDraw();
+$graph = new CLineGraphDraw(getRequest('type'));
 $graph->setPeriod($timeline['period']);
 $graph->setSTime($timeline['stime']);
+
+// change how the graph will be displayed if more than one item is selected
+if (getRequest('batch')) {
+	// set a default header
+	$graph->setHeader(_('Item values'));
+
+	// hide triggers
+	$graph->showTriggers(false);
+}
 
 if (isset($_REQUEST['from'])) {
 	$graph->setFrom($_REQUEST['from']);
@@ -80,7 +104,13 @@ if (isset($_REQUEST['height'])) {
 if (isset($_REQUEST['border'])) {
 	$graph->setBorder(0);
 }
-$graph->addItem($_REQUEST['itemid'], GRAPH_YAXIS_SIDE_DEFAULT, CALC_FNC_ALL);
+
+foreach ($items as $item) {
+	$graph->addItem($item['itemid'], GRAPH_YAXIS_SIDE_DEFAULT, (getRequest('batch')) ? CALC_FNC_AVG : CALC_FNC_ALL,
+		rgb2hex(get_next_color(1))
+	);
+}
+
 $graph->draw();
 
 require_once dirname(__FILE__).'/include/page_footer.php';

frontends/php/events.php
@@ -64,10 +64,6 @@ $fields = array(
 	'hostid'=>			array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
 	'triggerid'=>		array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,		null),
 	'period'=>			array(T_ZBX_INT, O_OPT, null,	null,		null),
-	'dec'=>				array(T_ZBX_INT, O_OPT, null,	null,		null),
-	'inc'=>				array(T_ZBX_INT, O_OPT, null,	null,		null),
-	'left'=>			array(T_ZBX_INT, O_OPT, null,	null,		null),
-	'right'=>			array(T_ZBX_INT, O_OPT, null,	null,		null),
 	'stime'=>			array(T_ZBX_STR, O_OPT, null,	null,		null),
 	'load'=>			array(T_ZBX_STR, O_OPT, P_SYS,	NULL,		null),
 	'fullscreen'=>		array(T_ZBX_INT, O_OPT, P_SYS,	IN('0,1'),	null),
@@ -788,7 +784,7 @@ else {
 						'params' => array(
 							'itemid' => $item['itemid'],
 							'action' => in_array($item['value_type'], array(ITEM_VALUE_TYPE_FLOAT, ITEM_VALUE_TYPE_UINT64))
-								? 'showgraph' : 'showvalues'
+								? HISTORY_GRAPH : HISTORY_VALUES
 						)
 					);
 				}

frontends/php/history.php
@@ -25,7 +25,7 @@ require_once dirname(__FILE__).'/include/graphs.inc.php';
 
 $page['file'] = 'history.php';
 $page['title'] = _('History');
-$page['hist_arg'] = array('itemid', 'hostid', 'groupid', 'graphid', 'period', 'dec', 'inc', 'left', 'right', 'stime', 'action');
+$page['hist_arg'] = array('itemids', 'period', 'stime', 'action', 'graphtype');
 $page['scripts'] = array('class.calendar.js', 'gtlc.js', 'flickerfreescreen.js');
 $page['type'] = detect_page_type(PAGE_TYPE_HTML);
 
@@ -38,19 +38,16 @@ require_once dirname(__FILE__).'/include/page_header.php';
 
 // VAR	TYPE	OPTIONAL	FLAGS	VALIDATION	EXCEPTION
 $fields = array(
-	'itemid' =>			array(T_ZBX_INT, O_OPT, P_SYS,	DB_ID,	'!isset({favobj})'),
+	'itemids' =>		array(T_ZBX_INT, O_MAND, P_SYS,	DB_ID,	'isset({favobj})'),
 	'period' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'dec' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'inc' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'left' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
-	'right' =>			array(T_ZBX_INT, O_OPT, null,	null,	null),
 	'stime' =>			array(T_ZBX_STR, O_OPT, null,	null,	null),
 	'filter_task' =>	array(T_ZBX_STR, O_OPT, null,	IN(FILTER_TASK_SHOW.','.FILTER_TASK_HIDE.','.FILTER_TASK_MARK.','.FILTER_TASK_INVERT_MARK), null),
 	'filter' =>			array(T_ZBX_STR, O_OPT, null,	null,	null),
 	'mark_color' =>		array(T_ZBX_STR, O_OPT, null,	IN(MARK_COLOR_RED.','.MARK_COLOR_GREEN.','.MARK_COLOR_BLUE), null),
 	'cmbitemlist' =>	array(T_ZBX_INT, O_OPT, null,	DB_ID,	null),
 	'plaintext' =>		array(T_ZBX_STR, O_OPT, null,	null,	null),
-	'action' =>			array(T_ZBX_STR, O_OPT, P_SYS,	IN('"showgraph","showvalues","showlatest","add","remove"'), null),
+	'action' =>			array(T_ZBX_STR, O_OPT, P_SYS,	IN('"'.HISTORY_GRAPH.'","'.HISTORY_VALUES.'","'.HISTORY_LATEST.'","'.HISTORY_BATCH_GRAPH.'"'), null),
+	'graphtype' =>      array(T_ZBX_INT, O_OPT, null,   IN(array(GRAPH_TYPE_NORMAL, GRAPH_TYPE_STACKED)), null),
 	// ajax
 	'filterState' =>	array(T_ZBX_INT, O_OPT, P_ACT,	null,	null),
 	'favobj' =>			array(T_ZBX_STR, O_OPT, P_ACT,	null,	null),
@@ -119,21 +116,20 @@ if ($page['type'] == PAGE_TYPE_JS || $page['type'] == PAGE_TYPE_HTML_BLOCK) {
 /*
  * Actions
  */
-$_REQUEST['action'] = getRequest('action', 'showgraph');
-$_REQUEST['itemid'] = array_unique(zbx_toArray($_REQUEST['itemid']));
+$_REQUEST['action'] = getRequest('action', HISTORY_GRAPH);
 
 /*
  * Display
  */
 $items = API::Item()->get(array(
-	'itemids' => $_REQUEST['itemid'],
+	'itemids' => getRequest('itemids'),
 	'webitems' => true,
 	'selectHosts' => array('name'),
 	'output' => array('itemid', 'key_', 'name', 'value_type', 'hostid', 'valuemapid'),
 	'preservekeys' => true
 ));
 
-foreach ($_REQUEST['itemid'] as $itemid) {
+foreach (getRequest('itemids') as $itemid) {
 	if (!isset($items[$itemid])) {
 		access_deny();
 	}
@@ -142,20 +138,19 @@ foreach ($_REQUEST['itemid'] as $itemid) {
 $items = CMacrosResolverHelper::resolveItemNames($items);
 
 $item = reset($items);
-$host = reset($item['hosts']);
-$item['hostname'] = $host['name'];
 
 $data = array(
-	'itemids' => getRequest('itemid'),
+	'itemids' => getRequest('itemids'),
 	'items' => $items,
-	'item' => $item,
+	'value_type' => $item['value_type'],
 	'action' => getRequest('action'),
 	'period' => getRequest('period'),
 	'stime' => getRequest('stime'),
 	'plaintext' => isset($_REQUEST['plaintext']),
 	'iv_string' => array(ITEM_VALUE_TYPE_LOG => 1, ITEM_VALUE_TYPE_TEXT => 1),
 	'iv_numeric' => array(ITEM_VALUE_TYPE_FLOAT => 1, ITEM_VALUE_TYPE_UINT64 => 1),
-	'fullscreen' => $_REQUEST['fullscreen']
+	'fullscreen' => $_REQUEST['fullscreen'],
+	'graphtype' => getRequest('graphtype', GRAPH_TYPE_NORMAL)
 );
 
 // render view

frontends/php/hosts.php
@@ -825,7 +825,7 @@ else {
 	$hostsWidget->setRootClass('host-list');
 
 	// filter
-	$filterTable = new CTable('', 'filter');
+	$filterTable = new CTable('', 'filter filter-center');
 	$filterTable->addRow(array(
 		array(array(bold(_('Name')), SPACE._('like').NAME_DELIMITER), new CTextBox('filter_host', $filter['host'], 20)),
 		array(array(bold(_('DNS')), SPACE._('like').NAME_DELIMITER), new CTextBox('filter_dns', $filter['dns'], 20)),
@@ -842,7 +842,7 @@ else {
 	$divButtons = new CDiv(array($filterButton, SPACE, $resetButton));
 	$divButtons->setAttribute('style', 'padding: 4px 0;');
 
-	$filterTable->addRow(new CCol($divButtons, 'center', 4));
+	$filterTable->addRow(new CCol($divButtons, 'controls', 4));
 
 	$filterForm = new CForm('get');
 	$filterForm->setAttribute('name', 'zbx_filter');

frontends/php/include/blocks.inc.php
@@ -121,7 +121,7 @@ function getFavouriteGraphs() {
 
 	if ($data['simpleGraphs']) {
 		foreach ($data['simpleGraphs'] as $item) {
-			$favourites->addItem(new CLink($item['label'], 'history.php?action=showgraph&itemid='.$item['id']), 'nowrap');
+			$favourites->addItem(new CLink($item['label'], 'history.php?action='.HISTORY_GRAPH.'&itemids[]='.$item['id']), 'nowrap');
 		}
 	}
 

commit 71d6467d40cb7c4764f4e3372e331b8e160ef598

..F....... [ZBX-8847] fixed mass actions in lists only affecting the selected elements on the current page, broken in r48423

frontends/php/js/chkbxrange.js
@@ -327,7 +327,7 @@ var chkbxRange = {
 		var form = jQuery('#goButton').closest('form');
 		for (var key in this.selectedIds) {
 			if (!empty(this.selectedIds[key])) {
-				create_var(form.name, this.pageGoName + '[' + key + ']', key, false);
+				create_var(form.attr('name'), this.pageGoName + '[' + key + ']', key, false);
 			}
 		}
 		return true;